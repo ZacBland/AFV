41a1dcd0f5607ad7705cf15f1fbf91ab7bc20674
sjhjrcode <steven.howell@okstate.edu>
2024-02-08 21:51:52 -0600

    Update code base for earth api

--- google_api.py
+++ google_api.py
@@ -2,14 +2,38 @@ import ee
 import numpy as np
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-
+import geemap
 # Initialize the Earth Engine
 ee.Initialize()
-
+ee.Authenticate()
 # Define the area of interest with latitude and longitude bounds
 lat, lon =  36.162738, -97.086237   
 point = ee.Geometry.Point(lon, lat)
 
+
+image = ee.ImageCollection('LANDSAT/LC08/C01/T1') \
+    .filterBounds(point) \
+    .filterDate('2020-01-01', '2023-12-31') \
+    .sort('CLOUD_COVER') \
+    .first() 
+    #.select(['B1','B4', 'B3', 'B2']) 
+
+# Define a map centered on southern Maine.
+m = geemap.Map(center=[lat, lon], zoom=10)
+
+# Add the image layer to the map and display it.
+m.add_layer(
+    image, {'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 30000}, 'first'
+)
+
+# Define the file path for the output image
+out_file = './Tiff/Stillwater_Airport.tif'
+
+# Export the image
+geemap.ee_export_image(image, filename=out_file, scale=30, region=point.buffer(10000).bounds(), file_per_band=False)
+
+#display(m)
+'''
 # Get the satellite image data
 image = ee.ImageCollection('LANDSAT/LC08/C01/T1') \
     .filterBounds(point) \
@@ -17,21 +41,34 @@ image = ee.ImageCollection('LANDSAT/LC08/C01/T1') \
     .sort('CLOUD_COVER') \
     .first() \
     .select(['B1','B4', 'B3', 'B2'])  # RGB bands for Landsat 8
+'''
 
+
+'''
+# Get the satellite image data
+image = ee.ImageCollection('LANDSAT/LC08/C01/T1') \
+    .filterBounds(point) \
+    .filterDate('2020-01-01', '2020-12-31') \
+    .sort('CLOUD_COVER') \
+    .first() \
+    .select(['B1','B4', 'B3', 'B2'])  # RGB bands for Landsat 8
+'''
+# Set the region to download
 # Set the region to download
 region = image.geometry().bounds().getInfo()['coordinates']
 
 # Export the image to a Google Drive folder
-task = ee.batch.Export.image.toDrive(**{
-    'image': image,
-    'description': 'imageToDriveExample',
-    'folder': 'Example_folder',
-    'fileNamePrefix': 'Stillwater_Airport',
-    'scale': 30,
-    'region': region
-})
-task.start()
+task = ee.batch.Export.image.toDrive(
+    image= image,
+    description = 'imageToDriveExample',
+    folder= 'Tiff_Images',
+    fileNamePrefix= 'Stillwater_Airport',
+    region=region,
+    scale=30,
+    crs='EPSG:5070'
 
+)
+task.start()
 
 # Wait for the task to complete
 import time
